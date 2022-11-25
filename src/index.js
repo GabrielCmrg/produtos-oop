eede755b356222167ad3a562578c3699f6648c12
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-11-25 17:46:20 -0300

    feat: create Item abstract class

---
+++ src/index.js
@@ -0,0 +1,11 @@
+class Item {
+  constructor(category, description, price) {
+    if (this.constructor == Item) {
+      throw new Error("Abstract classes can't be instantiated.");
+    }
+
+    this.category = category;
+    this.description = description;
+    this.price = price;
+  }
+}
