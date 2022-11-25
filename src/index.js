c7f7cf3dc240c8b16c2c4d0c93fa282d4e9cc0ad
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-11-25 19:31:14 -0300

    feat: create order class

--- src/index.js
+++ src/index.js
@@ -67,3 +67,26 @@ class Water extends Item {
     super("Water", description, price);
   }
 }
+
+class Order {
+  constructor() {
+    this.items = [];
+  }
+
+  addItem(item) {
+    this.item.push(item);
+  }
+
+  getTotal() {
+    return this.items.reduce((sum, item) => sum + item.price, 0);
+  }
+
+  getTaxes() {
+    return this.items.reduce((sum, item) => {
+      if (item instanceof TaxItem) {
+        return sum + item.calculateTax(item.getTax());
+      }
+      return sum;
+    }, 0);
+  }
+}
