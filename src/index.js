cb4fe07cfc450b7e15eb149468e6e12801e055c1
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-11-25 17:50:26 -0300

    feat: add TaxItem class

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 class Item {
+  category;
+  description;
+  price;
+
   constructor(category, description, price) {
     if (this.constructor == Item) {
       throw new Error("Abstract classes can't be instantiated.");
@@ -7,5 +11,19 @@ class Item {
     this.category = category;
     this.description = description;
     this.price = price;
+  }
+}
+
+class TaxItem extends Item {
+  constructor(category, description, price) {
+    if (this.constructor == TaxItem) {
+      throw new Error("Abstract classes can't be instantiated.");
+    }
+
+    super(category, description, price);
+  }
+
+  calculateTax(tax) {
+    return this.price * tax;
   }
 }
