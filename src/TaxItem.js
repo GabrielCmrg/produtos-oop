c334e4dc273e7322cbfa7a442c751b672e85d056
GabrielC <gabriel.camargo96@hotmail.com.br>
2022-11-25 19:49:36 -0300

    fix: call super before this

--- src/TaxItem.js
+++ src/TaxItem.js
@@ -2,11 +2,11 @@ import Item from "./Item.js";
 
 export default class TaxItem extends Item {
   constructor(category, description, price) {
+    super(category, description, price);
+
     if (this.constructor == TaxItem) {
       throw new Error("Abstract classes can't be instantiated.");
     }
-
-    super(category, description, price);
   }
 
   getTax() {
